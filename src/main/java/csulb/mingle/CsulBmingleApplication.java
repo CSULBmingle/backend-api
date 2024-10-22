60879d66aca2c8f88356adc40fb75cdc03225ee5
Jiihyun <cwlgusc@gmail.com>
2024-10-23 02:12:14 +0900

    feat: make users entity - create abstract BaseTimeEntity with createdAt field - enable JPA auditing - extend User entity from BaseTimeEntity

--- src/main/java/csulb/mingle/CsulBmingleApplication.java
+++ src/main/java/csulb/mingle/CsulBmingleApplication.java
@@ -2,8 +2,10 @@ package csulb.mingle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CsulBmingleApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/csulb/mingle/domain/user/entity/User.java
@@ -0,0 +1,55 @@
+package csulb.mingle.domain.user.entity;
+
+import csulb.mingle.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@SoftDelete(columnName = "is_deleted")
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 128)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String firstname;
+
+    @Column(nullable = false, length = 50)
+    private String lastname;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false, length = 50)
+    private String major;
+
+    @Column(nullable = false, length = 50)
+    private String minor;
+
+    @Column(name = "graduation_year", nullable = false)
+    private LocalDate graduationYear;
+
+    @Column(name = "student_id", nullable = false, length = 50)
+    private String studentId;
+
+    @Column(name = "password_changed_at")
+    private LocalDateTime passwordChangedAt;
+}
